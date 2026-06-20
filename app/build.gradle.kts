a9fbbda93d2caba890d21743fb8c1280d68a619e
FSXplay <skinoftheduck@gmail.com>
2026-06-20 11:53:53 +0800

    Install dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
 
 dependencies {
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
